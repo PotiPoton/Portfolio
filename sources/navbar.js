afaec64c168dd3b25c89adf0434b4d46627d1603
PotiPoton <leo.gilblas@mailo.com>
2024-10-11 10:47:43 +0200

    navbar générer js

--- sources/navbar.js
+++ sources/navbar.js
@@ -1,27 +1,62 @@
-// Sélectionner tous les liens de la navbar
-const tabLinks = document.querySelectorAll('.tab-link');
+function navbar(navElements){
+    let nav = document.createElement('nav')
+    nav.setAttribute('id', 'navbar');
+    let h1 = document.createElement('h1');
+    h1.innerHTML = "Léo <br> Gilblas";
+    
+    nav.appendChild(h1);
 
-// Boucler sur chaque lien et ajouter un écouteur d'événement
-tabLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault(); // Empêcher le comportement par défaut du lien (rechargement)
+    Object.entries(navElements).forEach(([dataTab, Affichage]) => {
+        let a = document.createElement('a');
+        if (dataTab == 'home') a.setAttribute('id', 'active');
+        a.setAttribute('href', '#');
+        a.setAttribute('class', 'tab-link');
+        a.setAttribute('data-tab', dataTab);
+        a.innerText = Affichage;
 
-    // Cacher tout le contenu des onglets
-    document.querySelectorAll('.tab-content').forEach(tab => {
-      tab.style.display = 'none';
+        nav.appendChild(a);
     });
 
-    // Afficher le contenu correspondant
-    const tabId = this.getAttribute('data-tab');
-    document.getElementById(tabId).style.display = 'block';
+    document.body.appendChild(nav);
+}
 
-    // Supprimer l'ID 'active' de l'ancien onglet actif
-    const activeLink = document.getElementById('active');
-    if (activeLink) {
-      activeLink.removeAttribute('id');
-    }
+function displayTabs(){
+    // Sélectionner tous les liens de la navbar
+    const tabLinks = document.querySelectorAll('.tab-link');
 
-    // Ajouter l'ID 'active' à l'onglet cliqué
-    this.setAttribute('id', 'active');
-  });
-});
+    // Boucler sur chaque lien et ajouter un écouteur d'événement
+    tabLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Empêcher le comportement par défaut du lien (rechargement)
+
+            // Cacher tout le contenu des onglets
+            document.querySelectorAll('.tab-content').forEach(tab => {
+                tab.style.display = 'none';
+            });
+
+            // Afficher le contenu correspondant
+            const tabId = this.getAttribute('data-tab');
+            document.getElementById(tabId).style.display = 'block';
+
+            // Supprimer l'ID 'active' de l'ancien onglet actif
+            const activeLink = document.getElementById('active');
+            if (activeLink) {
+            activeLink.removeAttribute('id');
+            }
+
+            // Ajouter l'ID 'active' à l'onglet cliqué
+            this.setAttribute('id', 'active');
+        });
+    });
+}
+
+let navElements = {
+    "home": "Accueil",
+    "tab2": "test button",
+    "parcours": "Mon parcours",
+    "cv": "CV",
+}
+navbar(navElements)
+displayTabs();
+
+
